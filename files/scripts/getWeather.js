d1b64729cf3a120321ccfd872603da1f76fbba98
Yurii <yurii.rudenko26@gmail.com>
2023-10-12 19:11:27 +0300

    added-https

--- files/scripts/getWeather.js
+++ files/scripts/getWeather.js
@@ -1,7 +1,7 @@
 import { renderCity } from "./renderCity.js"
 
 export function getWeather(city) {
-    let url = `http://api.weatherapi.com/v1/forecast.json?key=9ab729aaa6834a76be0115437230910&q=${city}&days=2&aqi=no&alerts=no`
+    let url = `https://api.weatherapi.com/v1/forecast.json?key=9ab729aaa6834a76be0115437230910&q=${city}&days=2&aqi=no&alerts=no`
     fetch(url)
         .then((response) => response.json())
         .then(data => renderCity(data))
